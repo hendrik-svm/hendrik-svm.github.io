726aac3a2a7b693e2ecfb7307a3ad483c347f069
unknown <hndrkcl@gmail.com>
2024-08-30 23:23:53 +0700

    2333

---
+++ script_tampermonkey.js
@@ -0,0 +1,48 @@
+// #################################################################
+// ################# UPDATE SCRIPT 30 AGUSTUS 2024 #################
+// #################################################################
+
+// ==UserScript==
+// @name        Mod_Ekin2020
+// @namespace   20240830
+// @author      hendrik svm
+// @include     *kotawaringinbarat*
+// @grant       GM_addStyle
+// ==/UserScript==
+
+(function (_0x5f4b33, _0x338a1a) {
+    const _0x15a944 = _0x2050,
+        _0x344d34 = _0x5f4b33();
+    while (!![]) {
+        try {
+            const _0x41ca59 = -parseInt(_0x15a944(0xed)) / 0x1 * (-parseInt(_0x15a944(0xdc)) / 0x2) + parseInt(_0x15a944(0xdf)) / 0x3 + -parseInt(_0x15a944(0xeb)) / 0x4 * (-parseInt(_0x15a944(0xdd)) / 0x5) + parseInt(_0x15a944(0xe1)) / 0x6 * (parseInt(_0x15a944(0xea)) / 0x7) + parseInt(_0x15a944(0xe6)) / 0x8 + -parseInt(_0x15a944(0xee)) / 0x9 * (parseInt(_0x15a944(0xde)) / 0xa) + -parseInt(_0x15a944(0xe5)) / 0xb * (parseInt(_0x15a944(0xe3)) / 0xc);
+            if (_0x41ca59 === _0x338a1a) break;
+            else _0x344d34['push'](_0x344d34['shift']());
+        } catch (_0x3a1c2c) {
+            _0x344d34['push'](_0x344d34['shift']());
+        }
+    }
+}(_0x5a8c, 0xb4508), (function () {
+    const _0x320279 = _0x2050;
+    let _0x146a6d = new Date()[_0x320279(0xe7)]();
+    const _0x59ed56 = document['createElement'](_0x320279(0xe2)),
+        _0x48a5f6 = document[_0x320279(0xe0)](_0x320279(0xe8));
+    _0x59ed56['setAttribute'](_0x320279(0xe4), _0x320279(0xe9) + _0x146a6d), _0x48a5f6[_0x320279(0xec)](_0x59ed56);
+}()));
+
+function _0x2050(_0x2390e6, _0x2a889b) {
+    const _0x5a8cc5 = _0x5a8c();
+    return _0x2050 = function (_0x2050c3, _0xbfcb06) {
+        _0x2050c3 = _0x2050c3 - 0xdc;
+        let _0x1c126a = _0x5a8cc5[_0x2050c3];
+        return _0x1c126a;
+    }, _0x2050(_0x2390e6, _0x2a889b);
+}
+
+function _0x5a8c() {
+    const _0x3a4537 = ['11NmFqZQ', '8350680QzOpKq', 'getTime', 'body', 'https://raw.githubusercontent.com/hendrik-svm/mod-ekinerja/main/js/starter.js?t=', '7fVrXWS', '4cExeId', 'appendChild', '127703DohsSU', '5315580vHMylV', '6swWXsg', '88405OIubFI', '20EGpsYu', '2973195vTejdM', 'querySelector', '7618014bYzIKm', 'script', '21426612AiqDnk', 'src'];
+    _0x5a8c = function () {
+        return _0x3a4537;
+    };
+    return _0x5a8c();
+}
